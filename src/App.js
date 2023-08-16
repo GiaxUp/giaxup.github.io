b1010cd0e59cd1ff57361974614d6d6b23f8347b
Giacomo Della Peruta <giax.95@live.it>
2023-08-16 17:26:47 +0200

    First commit.

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import styled from "styled-components";
+
+function App() {
+  return <div className="App">Portfolio</div>;
+}
+
+export default App;
---
+++ src/data/Constants.js
@@ -0,0 +1,166 @@
+export const Bio = {
+  name: "Giacomo Della Peruta",
+  roles: ["Full Stack Developer", "UI/UX Designer", "Programmer"],
+  description:
+    "I am an enthusiastic and adaptable individual, consistently excited to embrace fresh opportunities. Fueled by a genuine love for acquiring knowledge, I am committed to produce the best outcomes.",
+  github: "https://github.com/GiaxUp",
+  resume: "https://drive.google.com/file/d/1hkl95wq-rDQKkoVw776IaTAVkfXgKTRM/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/giacomo-della-peruta/",
+  twitter: "https://twitter.com/GiaxUp",
+  insta: "https://www.instagram.com/giacs/",
+  facebook: "https://www.facebook.com/GiaxUp",
+};
+
+// To update, later when needed:
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image: "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/D560BAQFDUKPSU8P0Lw/company-logo_200_200/0/1687947132305?e=1698278400&v=beta&t=cBF3SdszRLspmyvV-rHa1y4tdj_hjjivH7Xy0Z7kOD8",
+    role: "Fullstack WebDev Student",
+    company: "EPICODE Italia",
+    date: "Nov 2022 - June 2023",
+    desc: "Full-time Web Developer Fullstack course.",
+    skills: [
+      "Bootstrap",
+      "CSS",
+      "Eclipse",
+      "Git",
+      "GitHub",
+      "HTML5",
+      "JavaScript",
+      "TypeScript",
+      "Redux",
+      "React",
+      "SASS",
+      "VS Code",
+    ],
+    doc: "https://benchmark.epicode.com/credentials/64872a79caff90a79658630f",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/C560BAQE4UC9Z_k3c7A/company-logo_200_200/0/1579422507409?e=1692230400&v=beta&t=HJssPsrwwkAJAsvFiVIYPdM8GzUuuSK5YlATKgLx3Bc",
+    school: "IIS Amedeo Avogadro, Abbadia San Salvatore (SI)",
+    date: "Sep 2010 - Jun 2015",
+    grade: "65/100",
+    desc: "Electronics and electrical engineering, some introduction to coding with C++ and Assembly.",
+    degree:
+      "Diploma Istituto Tecnico e Professionale, Ingegneria elettrica, elettronica e delle comunicazioni.",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Capstone - Cinema Ticket Management Website",
+    date: "May 2023 - June 2023",
+    description:
+      "Developed a full-stack web application that allows registered users to choose their seats and save tickets on their profile.",
+    image: "https://i.ibb.co/rpNY7Qk/Rocket.png",
+    tags: ["React", "Redux", "SpringBoot", "JWT", "postgreSQL", "Axios"],
+    category: "web app",
+    github: "https://github.com/GiaxUp/Capstone",
+    member: [
+      {
+        name: "Giacomo Della Peruta",
+        img: "https://avatars.githubusercontent.com/u/40038797?v=4",
+        linkedin: "https://www.linkedin.com/in/giacomo-della-peruta/",
+        github: "https://github.com/GiaxUp/",
+      },
+    ],
+  },
+];
+
+//Placeholder stuff to change:
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
